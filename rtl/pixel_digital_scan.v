c5a662547fe9ff4cb415c9413a14f3fa5456977f
bhyou <bhyou@foxmail.com>
2021-12-06 12:22:16 +0800

    parameterize

--- rtl/pixel_digital_scan.v
+++ rtl/pixel_digital_scan.v
@@ -1,99 +1,74 @@
 `timescale 1ns/100ps
-module pixel_digital_scan (clk_s,rst_s,start_s,speak_s,marker_a,rowSel,columnSel);
+module pixel_digital_scan #(
+	parameter ROW_LENGTH    = 32,
+	parameter COLUMN_LENGTH = 8
+)(
+	input  wire                     clock_i,
+	input  wire                     reset_i,
+	input  wire                     start_i,
+	input  wire                     speak_i,
+	output wire                     marker_o,
+	output wire [ROW_LENGTH-1:0]    rowSel_o,
+	output wire [COLUMN_LENGTH-1:0] columnSel_o
+);
 
-parameter ROW = 400;
-parameter COLUMN = 32;
-localparam RowCntWid = $clog2(ROW) ;    
-localparam ColCntWid = $clog2(COLUMN); 
-
-input                   clk_s;
-input                   rst_s;
-input                   start_s;
-input                   speak_s;
-output                  marker_a;
-output [ROW-1:0]        rowSel;
-output [COLUMN-1:0]     columnSel;
-
+localparam RowIndex    = $clog2(ROW_LENGTH) ;    
+localparam ColumnIndex = $clog2(COLUMN_LENGTH); 
 
 reg                      start_delay;
 reg                      started;
-reg [ROW-1:0]            row_selection_buf;
-reg [COLUMN-1:0]         column_selection_buf;
-reg [ColCntWid:0]        col_cnt;
-reg [RowCntWid:0]        row_cnt;
+reg [ROW_LENGTH-1:0]     row_selection_buf;
+reg [COLUMN_LENGTH-1:0]  column_selection_buf;         
+reg [ColumnIndex:0]      col_cunt;
+reg [RowIndex:0]         row_cunt;
 
-always @ (posedge clk_s or posedge rst_s) begin
-	if(rst_s) begin
-		col_cnt<=0;
-		row_cnt<=0;
-		started<=0;
-		start_delay<=0;
-	end
-	else begin
-		start_delay<=start_s;
-		if((!start_delay)&start_s) begin
-			started<=1'b1;
-			col_cnt<=0;
-			row_cnt<=0;
-		end
-		else if (speak_s&started) begin
-			if (col_cnt==COLUMN-1) begin
-				col_cnt<=0;				
-			  if (row_cnt==ROW-1) begin
-					row_cnt<=0;
-				end
-				else begin
-					row_cnt<=row_cnt+1;
-				end
-			end
-			else begin
-				col_cnt<=col_cnt+1;
-			end	
+always @ (posedge clock_i or posedge reset_i) begin
+	if(reset_i) begin
+		col_cunt   <=  0;
+		row_cunt   <=  0;
+		started    <=  0;
+		start_delay<=  0;
+	end	else begin
+		start_delay <= start_i;
+		if((!start_delay) & start_i) begin
+			started  <= 1;
+			col_cunt <= 0;
+			row_cunt <= 0;
+		end else if (speak_i & started) begin
+			col_cunt <= (col_cunt==COLUMN_LENGTH-1) ? 0 : col_cunt + 1;
+			row_cunt <= (row_cunt==ROW_LENGTH-1) ? 0 : 
+					    (col_cunt==COLUMN_LENGTH-1) ? row_cunt + 1:
+						row_cunt;
 		end		
 	end
 end
 
-always @ (posedge clk_s or posedge rst_s)
-begin
-	if(rst_s) begin
+always @ (posedge clock_i or posedge reset_i) begin
+	if(reset_i) begin
 		column_selection_buf<=0;
-	end
-	else begin
-		if(col_cnt==0) begin
+	end	else begin
+		if(col_cunt==0) begin
 			column_selection_buf<=1;
-		end
-		else if (speak_s) begin
-			column_selection_buf<={column_selection_buf[COLUMN-2:0],column_selection_buf[COLUMN-1]};
+		end else if (speak_i) begin
+			column_selection_buf<={column_selection_buf[COLUMN_LENGTH-2:0],column_selection_buf[COLUMN_LENGTH-1]};
 		end
 	end
 end
 
-always @ (posedge clk_s or posedge rst_s)
-begin
-	if(rst_s) begin
+always @ (posedge clock_i or posedge reset_i) begin
+	if(reset_i) begin
 		row_selection_buf <= 0;
-	end
-	else begin
-		if(row_cnt==0) begin
+	end	else begin
+		if(row_cunt==0) begin
 			row_selection_buf <= 1;
-		end
-		else if (speak_s & (col_cnt==0)) begin
-			row_selection_buf <= {row_selection_buf[ROW-2:0],row_selection_buf[ROW-1]};
+		end else if (speak_i & (col_cunt==0)) begin
+			row_selection_buf <= {row_selection_buf[ROW_LENGTH-2:0],row_selection_buf[ROW_LENGTH-1]};
 		end
 	end
 end
 
-assign rowSel    = row_selection_buf;
-
-generate 
-   genvar i,j;
-   for(j=0; j<1; j=j+1) begin:group
-     for(i=0; i< COLUMN; i=i+1) begin:colSel
-       assign columnSel[j*COLUMN+i] = {column_selection_buf[i]};
-     end
-   end
-endgenerate
-
-assign marker_a=((col_cnt==1)&(row_cnt==0))? 1'b1 : 1'b0;
+assign marker_o    = ((col_cunt==1) & (row_cunt==0))? 1'b1 : 1'b0;
+assign rowSel_o    = row_selection_buf;
+assign columnSel_o = column_selection_buf;
 
 endmodule
--- tbench/pixel_digital_scan_tb.v
+++ tbench/pixel_digital_scan_tb.v
@@ -1,116 +1,86 @@
 `timescale 1ns / 100ps
 module pixel_digital_sca_tb;
+   parameter period = 40;
+   parameter row = 32;
+   parameter col = 16;
 
-`define Topmetal_verilog
-parameter period = 40;
-parameter row = 400;
-parameter col = 32;
-parameter row_width = $clog2(row);    
-parameter col_width = $clog2(col); 
+   reg            clock ;
+   reg            reset  ;
+   reg            start  ;
+   reg            speak  ;
+   wire           marker ;
+   wire [row-1:0] row_sel;
+   wire [col-1:0] col_sel;
 
-reg            clk_tb ;
-reg            reset  ;
-reg            start  ;
-reg            speak  ;
-wire           marker ;
-wire [row-1:0] row_sel;
-wire [col*32-1:0] col_sel;
-
-`ifdef Topmetal_verilog
    pixel_digital_scan #(
-    .ROW(row), 
-  	 .COLUMN(col),
-  	 .ROW_CNT_WIDTH(row_width),
-  	 .COL_CNT_WIDTH(row_width)) dut(
-      .clk_s            (clk_tb ),
-      .rst_s            (reset  ),
-      .start_s          (start  ),
-      .speak_s          (speak  ),
-      .marker_a         (marker ),
-      .rowSel           (row_sel),
-      .columnSel        (col_sel)
-  		);
-`else
-top_metal_digital_scan #(
-   .ROW_LENGTH(row), 
-	 .ANALOG_COLUMN_LENGTH(col),
-	 .ROW_CNT_WIDTH(row_width),
-	 .COL_CNT_WIDTH(row_width))
-	 dut_vhdl (
-    .clk_s            (clk_tb ),
-    .rst_s            (reset  ),
-    .start_s          (start  ),
-    .speak_s          (speak  ),
-    .marker_a         (marker ),
-    .ROW_SELECTION    (row_sel),
-    .COLUMN_SELECTION (col_sel)
-		);
-`endif
+    .ROW_LENGTH(row), 
+  	 .COLUMN_LENGTH(col)
+  	) dut(
+      .clock_i          (clock ),
+      .reset_i          (reset  ),
+      .start_i          (start  ),
+      .speak_i          (speak  ),
+      .marker_o         (marker ),
+      .rowSel_o         (row_sel),
+      .columnSel_o      (col_sel)
+  	);
 
-
-  initial begin
-   `ifdef PRE
-       $sdf_annotate("../syn/output/pixel_digital_scan_DC.sdf",pixel_digital_sca_tb.dut);
-   `endif
-   
-   `ifdef POST
-       $sdf_annotate("../tbench/pixel_digital_scan_tt.sdf",pixel_digital_sca_tb.dut);
-   `endif
-   
-   `ifdef POST_FF
-       $sdf_annotate("../tbench/pixel_digital_scan_tt.sdf",pixel_digital_sca_tb.dut);
-   `endif
-  end
+   initial begin
+   	clock = 1'b1;
+     forever #(period/2) clock = ~ clock;
+   end
 
 
 
-initial begin
-	clk_tb = 1'b1;
-	repeat(700)  #period clk_tb = ~ clk_tb;
-   #(period*3);
-  forever #period clk_tb = ~ clk_tb;
-   
-end
-
-
-
-initial begin
-   fork
-      resetPluse(50);
-      startPluse(60);
-      speakPluse(70);
-   join
-   repeat(1000)  @(posedge clk_tb);	 
-	 $stop();
-end
-   task resetPluse(integer xcycle);
-	   #0 reset = 1'b1;
-	   #(period*2+10) reset = 1'b0;
-      repeat(xcycle) @(posedge clk_tb);
+   //  start: soft-reset, posedge active
+   //  speak: run / stop, level active
+   //  reset: hard-reset, level active
+   initial begin
+      // hard-reset process
       reset = 1'b1;
-	   #(period*2+10) reset = 1'b0;
-   endtask
+      start = 1'b0;
+      speak = 1'b0;
+      repeat(4) @(negedge clock);
 
-   task startPluse(integer xcycle);
-	   #0 start = 1'b0;
-      repeat(2) @(posedge clk_tb);
-	   start = #6 1'b1;
-      repeat(1) @(posedge clk_tb);
-      start = #6 0;
-      repeat(xcycle) @(posedge clk_tb);
-      start = #6 1;
-      repeat(1) @(posedge clk_tb);
-      start = #6 0;
-   endtask
+      // setup process
+      reset = 1'b0;
+      start = 1'b1;
+      speak = 1'b1;
+      repeat(50) @(negedge clock);
 
-   task speakPluse(integer xcycle);
-		#0 speak = 1'b0;
-      repeat(4) @(posedge clk_tb);
-	   speak = #6 1'b1;
-      repeat(xcycle) @(posedge clk_tb);
-      speak = #6 1'b0;
-      @(posedge clk_tb);
-      speak = #6 1'b1;
-   endtask
+      // stop scan process
+      reset = 1'b0;
+      start = 1'b1;
+      speak = 1'b0;
+      repeat(1) @(negedge clock);
+
+      // go-on scan process
+      reset = 1'b0;
+      start = 1'b1;
+      speak = 1'b1;
+      repeat(10) @(negedge clock);
+
+      // soft-reset process
+      reset = 1'b0;
+      start = 1'b0;
+      speak = 1'b1;
+      repeat(1) @(negedge clock);
+      reset = 1'b0;
+      start = 1'b1;
+      speak = 1'b1;
+      repeat(1) @(negedge clock);
+
+      // go-on scan process
+      reset = 1'b0;
+      start = 1'b0;
+      speak = 1'b1;
+      repeat(20) @(negedge clock);
+
+
+
+
+      repeat(100)  @(posedge clock);	 
+   	 $stop();
+   end
 
 endmodule
